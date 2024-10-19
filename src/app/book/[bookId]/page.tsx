2b81573b946ead6b2cf916f68cc0d436209f977d
= <=>
2024-10-20 01:24:52 +0500

    add single book page

---
+++ src/app/book/[bookId]/page.tsx
@@ -0,0 +1,47 @@
+import { Book } from '@/types'
+import Image from 'next/image'
+import React from 'react'
+
+const SingleBookPage = async ({ params }: { params: { bookId: string } }) => {
+    let book: Book | null = null;
+    try {
+        const response = await fetch(`${process.env.BASE_URL}/books/${params.bookId}`);
+        if (!response.ok) {
+            throw new Error('Error fetching single book in if condition');
+        }
+        book = await response.json();
+
+    } catch (error) {
+        throw new Error('Error fetching single book')
+    }
+
+    if (!book) {
+        throw new Error('Book not found')
+    }
+
+
+    return (
+        <div className='container grid grid-cols-3 gap-10 py-2'>
+            <div className='col-span-2 pr-16 text-primary-50'>
+                <h2 className='mb-5 text-5xl font-bold leading-[1.1]'>{book.title}</h2>
+                <span className='font-semibold'>by {book.author.name}</span>
+                {/* <p className='mt-5 text-lg leading-8'>{book.description}</p> */}
+
+            </div>
+            <div className='flex justify-end'>
+                <Image
+                    src={book.coverImage}
+                    alt={book.title}
+                    className='rounded-md border'
+                    height={0}
+                    width={0}
+                    sizes='100vw'
+                    style={{ width: 'auto', height: 'auto' }}
+
+                />
+            </div>
+        </div>
+    )
+}
+
+export default SingleBookPage
