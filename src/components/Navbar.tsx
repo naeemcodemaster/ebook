0ed1382b654ae94b03dc50dcd58bb2f105794c0c
= <=>
2024-10-20 10:56:26 +0500

    navbar link changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
             </Link>
         </div>
         <div className='flex items-center gap-4'>
-            <button className='h-10 rounded-md border border-primary-100 px-4 py-2 text-sm font-medium text-primary-100 transition-all hover:border-primary-100 hover:bg-primary-100 hover:text-white active:border-primary-100 active:text-white active:bg-primary-100'>Sign in</button>
+            <button className='h-10 rounded-md border border-primary-100 px-4 py-2 text-sm font-medium text-primary-100 transition-all hover:border-primary-100 hover:bg-primary-100 hover:text-white active:border-primary-100 active:text-white active:bg-primary-100'>Sign Up</button>
 
             <button className='h-10 rounded-md bg-primary-100 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-50 active:bg-primary-50'>Sign in</button>
         </div>
